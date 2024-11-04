ccde5e26b1aad83507e94accf29257f10798eade
aamirk0008 <113338901+aamirk0008@users.noreply.github.com>
2024-11-04 13:32:12 +0530

    Project Setup

---
+++ src/routers/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            }
+        ]
+    }
+])
+
+
+export default router
