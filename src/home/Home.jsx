2e62c612a4a4fae11886893e5368784b347b0bf3
aamirk0008 <113338901+aamirk0008@users.noreply.github.com>
2024-11-04 14:25:50 +0530

    Project Setup 1.2

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section id='browse-by-location'>
+      <div>
+        <h1 className='text-4xl font-bold'>Browse By <span className='text-primary'>Location</span></h1>
+      </div>
+
+    </section>
   )
 }
 
