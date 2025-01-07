05c8e09ae920282ce10c931ec8a2af0336b20120
Anurag Patel <118664591+anurag-2024@users.noreply.github.com>
2025-01-07 17:04:05 +0530

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Montserrat', 'sans-serif'],
-      },
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
