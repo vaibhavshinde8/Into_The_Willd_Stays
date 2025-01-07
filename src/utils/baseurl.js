5ae19554cd70f33c2a55ce3298670c265b66c75d
Anurag Patel <118664591+anurag-2024@users.noreply.github.com>
2025-01-07 16:35:41 +0530

    backedn url update

--- src/utils/baseurl.js
+++ src/utils/baseurl.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://intothewilds-backend.onrender.com/api/v1";
+// export const BASE_URL = "https://intothewilds-backend.onrender.com/api/v1";
+export const BASE_URL = "https://intothewilds-backend-ghye.onrender.com/api/v1";
 // export const BASE_URL = "http://localhost:5000/api/v1";
