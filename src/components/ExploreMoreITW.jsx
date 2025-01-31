4b44b103c28586898ad487ab561c2b56f55c7a3a
aamirk0008 <113338901+aamirk0008@users.noreply.github.com>
2025-01-31 14:34:24 +0530

    correction

--- src/components/ExploreMoreITW.jsx
+++ src/components/ExploreMoreITW.jsx
@@ -501,7 +501,7 @@ Thank you"`,
                 key={button.id}
                 href={`#${button.id}`}
                 onClick={() => setActiveButton(button.id)}
-                className={`py-2  rounded font-semibold flex-auto  flex items-center justify-center px-4 transition duration-300 ${activeButton === button.id
+                className={`py-2  rounded font-semibold flex-auto border-2 border-gray-200 shadow-lg flex items-center justify-center px-4 transition duration-300 ${activeButton === button.id
                   ? "bg-[#163257] text-white"
                   : "bg-white text-gray-800"
                   }`}
